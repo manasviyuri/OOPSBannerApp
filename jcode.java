6adf29a1c4e774bf2648cdbf3e57104907a24df6
manasviyuri <mk2393@srmist.edu.in>
2026-02-17 21:59:52 +0530

    Added jcode

---
+++ jcode.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; // Import the Scanner class to get user input
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        scanner.close();
+    }
+}
