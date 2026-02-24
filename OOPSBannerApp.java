7c803086186855628d0b099b436fa76b740f5e91
manasviyuri <mk2393@srmist.edu.in>
2026-02-24 23:12:34 +0530

    UC6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,48 @@
-public class OOPSBannerApp{
-    public static void main(String[] args)
-	{
-		
-       String[] lines = {
-         String.join(" " , "   ***   " , "*******" , "*******" , "*******");
-	     String.join(" " , " **   ** " , "**   **" , "**   **" , "**     ");
-	     String.join(" " , "**     **" , "**   **" , "**   **" , "**     ");
-	     String.join(" " , "**     **" , "*******" , "*******" , "*******");
-	     String.join(" " , "**     **" , "**     " , "**     " , "     **");
-	     String.join(" " , " **   ** " , "**     " , "**     " , "     **");
-	     String.join(" " , "   ***   " , "**     " , "**     " , "*******");
-	   };
-	    for (String line : lines) {
+public class OOPSBannerApp {
+
+    public static String[] getO() {
+        return new String[] {
+            "  ***  ",
+            " *   * ",
+            " *   * ",
+            " *   * ",
+            " *   * ",
+            "  ***  "
+        };
+    }
+
+    public static String[] getP() {
+        return new String[] {
+            " ***** ",
+            " *   * ",
+            " ***** ",
+            " *     ",
+            " *     ",
+            " *     "
+        };
+    }
+
+    public static String[] getS() {
+        return new String[] {
+            "  ***** ",
+            " *      ",
+            "  ***** ",
+            "      * ",
+            "      * ",
+            " *****  "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
+
+
+        for (int i = 0; i < O.length; i++) {
+            String line = String.join("   ", O[i], P[i], P[i], S[i]);
             System.out.println(line);
-		}
-	   
-	}
+        }
+    }
 }
